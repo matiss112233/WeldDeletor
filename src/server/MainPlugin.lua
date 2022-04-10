5b00267287a92e4f832399aa8b9191b3c645f595
matiss112233 <eeoson4@gmail.com>
2022-04-10 12:47:56 -0400

    Added Widget info, making widget.

--- src/server/MainPlugin.lua
+++ src/server/MainPlugin.lua
@@ -4,15 +4,20 @@ end
 
 local Plugin = plugin:CreateToolbar("Weld Deletor")
 local Button = plugin:CreateButton("Weld Deletor", "An easy-to-use tool that deletes all Welds from workspace!", "")
+local WidgetInfo = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Left, false, false, 200, 300, 150, 150)
+local Widget = plugin:CreateDockWidgetPluginGui("Weld Deletor", WidgetInfo)
+local PluginEnabled = false
 
 -- TODO: Implement ChangeHistoryService
--- local ChangeHistoryService = game:GetService("ChangeHistoryService")
+local ChangeHistoryService = game:GetService("ChangeHistoryService")
 
 Button.MouseClick:Connect(function(Click)
+    PluginEnabled = true
+    Widget.Enabled = true
+    
     for _, v in ipairs(workspace:GetDescendants()) do
         if v:IsA("Weld") then
             v:Destroy()
-            print("All Welds deleted!")
         end
     end
 end)
